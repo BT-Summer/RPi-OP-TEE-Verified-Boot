0e8976064d6434ae25802902c4abd417b647c17c
Thomas Gymer <thomas.gymer@bt.com>
2024-08-14 10:42:27 +0100

    update dtb location

--- image-build.py
+++ image-build.py
@@ -5,6 +5,8 @@ with open("optee/build/Makefile", "r") as f:
 	for line in f:
 		if "@install -v -p --mode=755 $(LINUX_IMAGE) $(BOOT_PARTITION_FILES)/kernel8.img" in line:
 			new_lines.append("\t@install -v -p --mode=755 ../../image.fit $(BOOT_PARTITION_FILES)/image.fit\n")
+		elif "@install -v -p --mode=755 $(LINUX_DTB_RPI3_BPLUS) $(BOOT_PARTITION_FILES)/bcm2710-rpi-3-b-plus.dtb" in line:
+			new_lines.append("\t@install -v -p --mode=755 ../../bcm2837-rpi-3-b-plus-u-boot.dtb $(BOOT_PARTITION_FILES)/bcm2710-rpi-3-b-plus.dtb\n")
 		else:
 			new_lines.append(line)
 	
