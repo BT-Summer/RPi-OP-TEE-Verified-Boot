2987fd105ff0c734c26f4cfe8d0cfd72cfe9e088
Thomas Gymer <thomas.gymer@bt.com>
2024-08-20 16:52:41 +0100

    add check for algorithm names

--- tests/key-tests.py
+++ tests/key-tests.py
@@ -23,16 +23,30 @@ pub_key_len = cert.get_pubkey().bits()
 
 res = subprocess.run(["u-boot/tools/fdtgrep", "-n", "/signature", "-s", dtb_file], capture_output=True, encoding="UTF-8")
 
+raw_key_algo = re.search("algo = \"[a-z]+[0-9]+,[a-z]+[0-9]+\"", res.stdout).group()
+key_algo = raw_key_algo.split(",")[1].replace("\"", "")
+key_algo_len = int(re.search("[0-9]+", key_algo).group())
+
 # TODO: need to check if this would support other algorithms
 raw_key_len = re.search("num-bits = <0x[0-9a-f]{8}>;", res.stdout).group()
 key_len = int(raw_key_len[12:-2], 16)
+
+if key_len != key_algo_len:
+    sys.exit("DTB key length and algorithm do not match")
 
 # FIT FILE
 
 res = subprocess.run(["u-boot/tools/fdtgrep", "-n", "/configurations", "-s", fit_file], capture_output=True, encoding="UTF-8")
 
+raw_sig_algo = re.search("algo = \"[a-z]+[0-9]+,[a-z]+[0-9]+\"", res.stdout).group()
+sig_algo = raw_sig_algo.split(",")[1].replace("\"", "")
+sig_algo_len = int(re.search("[0-9]+", sig_algo).group())
+
 raw_sig_len = re.search("value = <(0x[0-9a-f]{8}[\s>])+;", res.stdout).group()
 sig_len = len(raw_sig_len[9:-2].split(" ")) * 32
+
+if sig_len != sig_algo_len:
+    sys.exit("FIT file signature length and signature algorithm do not match")
 
 # CHECKS
 
