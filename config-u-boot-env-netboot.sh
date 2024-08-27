cbd5b376f47e8a8aea13c2fc9c48e80bd673a55f
Thomas Gymer <thomas.gymer@bt.com>
2024-08-27 16:26:41 +0100

    fix u-boot bootargs for linux network boot

--- config-u-boot-env-netboot.sh
+++ config-u-boot-env-netboot.sh
@@ -18,20 +18,20 @@ ttyconsole=tty0
 gatewayip=192.168.1.1
 netmask=255.255.255.0
 nfsserverip=192.168.1.1
-nfspath=/srv/nfs/rootfs/
+nfspath=/srv/nfs/rootfs
 
 # bootcmd & bootargs configuration
 preboot=usb start
 bootcmd=run mmcboot
 set_bootargs_tty=setenv bootargs console=${ttyconsole} console=${sttyconsole},${baudrate}
-set_bootargs_nfs=setenv bootargs ${bootargs} root=/dev/nfs rw rootfstype=nfs nfsroot=${nfsserverip}:${nfspath},udp,nfsvers=4 ip=dhcp
+set_bootargs_nfs=setenv bootargs ${bootargs} root=/dev/nfs rw rootfs=nfs nfsroot=${nfsserverip}:${nfspath},tcp,nfsvers=4 ip=dhcp smsc95xx.macaddr= dma.dmachans=0x7f35 rootwait 8250.nr_uarts=1 fsck.repair=yes bcm2708_fb.fbwidth=1920 bcm2708_fb.fbheight=1080 vc_mem.mem_base=0x3ec00000 vc_mem.mem_size=0x40000000 dwc_otg.fiq_enable=0 dwc_otg.fiq_fsm_enable=0 dwc_otg.nak_holdoff=0
 set_common_args=setenv bootargs ${bootargs} smsc95xx.macaddr=${ethaddr} 'dma.dmachans=0x7f35 rootwait 8250.nr_uarts=1 fsck.repair=yes bcm2708_fb.fbwidth=1920 bcm2708_fb.fbheight=1080 vc_mem.mem_base=0x3ec00000 vc_mem.mem_size=0x40000000 dwc_otg.fiq_enable=0 dwc_otg.fiq_fsm_enable=0 dwc_otg.nak_holdoff=0'
 
 # fit boot
 load_fit=fatload mmc 0:1 0x02000000 image.fit
-load_fit_tftp=bootp 0x02000000 ${nfsserverip}:image.fit
+load_fit_tftp=bootp 0x02000000 ${nfsserverip} image.fit
 boot_fit=bootm 0x02000000
-mmcboot=run load_fit-tftp; run set_bootargs_tty set_common_args set_bootargs_nfs; run boot_fit
+mmcboot=run load_fit_tftp; run set_bootargs_nfs; run boot_fit
 " > optee/build/rpi3/firmware/uboot.env.txt
 
-
+# bootargs=smsc95xx.macaddr= dma.dmachans=0x7f35 rootwait 8250.nr_uarts=1 fsck.repair=yes bcm2708_fb.fbwidth=1920 bcm2708_fb.fbheight=1080 vc_mem.mem_base=0x3ec00000 vc_mem.mem_size=0x40000000 dwc_otg.fiq_enable=0 dwc_otg.fiq_fsm_enable=0 dwc_otg.nak_holdoff=0 root=/dev/nfs rw rootfs=nfs nfsroot=192.168.1.1:/srv/nfs/rootfs,tcp,nfsvers=4 ip=dhcp nfsrootdebug
